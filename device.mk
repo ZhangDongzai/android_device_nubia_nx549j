1a95bfdbe017dcb7e38e9e174b5511e182ad5cc8
Ethan Chen <intervigil@gmail.com>
2018-11-30 23:28:49 -0800

    nx549j: Add AOSP RCS packages

--- device.mk
+++ device.mk
@@ -326,6 +326,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+# RCS
+PRODUCT_PACKAGES += \
+    com.android.ims.rcsmanager \
+    PresencePolling \
+    RcsService
+
 # RIL
 PRODUCT_PACKAGES += \
     libcnefeatureconfig \
