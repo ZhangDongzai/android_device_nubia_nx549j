59ec9f2db1cd4dce1e7527c643b74b372aa6c558
mike dooley <mdooley@google.com>
2018-12-18 20:29:02 +0100

    nx549j: Enabling sound trigger V2_2 for msm8953 targets
    V2_2 has the same functionality as V2_1 except it adds one new
    api, getModelState.
    
    Note: V2_1 will be removed in a subsequent cl.
    
    Test: built android and tested using SoundTriggerTestApp
    
    Bug: 70206501

--- device.mk
+++ device.mk
@@ -50,7 +50,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-service \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.soundtrigger@2.2-impl
 
 # XML Audio configuration files
 PRODUCT_COPY_FILES += \
