8995b4b2c0a173851fa7a5786b61981f861ec255
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    nx549j: Convert configstore settings to props
    Change-Id: Iadefa31bc863b7ab70239874bed82bef240cbba5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -116,6 +116,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.data.mode=concurrent \
     persist.radio.apm_sim_not_pwdn=1
 
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.max_virtual_display_dimension=4096
+
 # Time services
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.delta_time.enable=true
