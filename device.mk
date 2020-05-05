c8c9b3088afe02e56d8c5cfe75d82b5e713957da
Pranav Vashi <neobuddy89@gmail.com>
2020-05-06 03:45:37 +0530

    nx549j: Add missing 64-bit interfaces required for IMS
    Change-Id: I8ef9171cd5d53cfb5ddfaae49b56696940ae6442

--- device.mk
+++ device.mk
@@ -79,11 +79,13 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
+    android.hardware.camera.device@3.4:64 \
+    android.hardware.camera.device@3.5:64 \
+    android.hardware.camera.provider@2.4:64 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    camera.device@3.2-impl \
-    vendor.qti.hardware.camera.device@1.0 \
-    vendor.qti.hardware.camera.device@1.0_vendor
+    android.hardware.camera.provider@2.5:64 \
+    vendor.qti.hardware.camera.device@1.0:64
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
