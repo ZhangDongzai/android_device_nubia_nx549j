0b362da7d6efb0fbddd50a74405d4cd6d982db7c
Ethan Chen <intervigil@gmail.com>
2020-10-11 15:07:35 -0700

    nx549j: Move telephony packages to /product
    Change-Id: I893eff0ca79bb6eb4ac53030eb6d1a3e1ad54b5b

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright 2018 The LineageOS Project
+# Copyright (C) 2018-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -80,13 +80,12 @@ $(LOCAL_BUILT_MODULE):
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
-
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_PRODUCT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /system/lib64/$(notdir $@) $@
+	$(hide) ln -sf /system/product/lib64/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
