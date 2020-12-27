de374953bd2a2b91fd7525bfcbf676c4041a7278
Michael Bestas <mkbestas@lineageos.org>
2020-12-28 01:17:48 +0200

    nx549j: Switch to standalone extract utils
    Align with templates while we are at it
    
    Change-Id: I3cd279191c7bd3582f1c1f80c588eb26cf9da933

--- extract-files.sh
+++ extract-files.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -25,123 +15,60 @@ export DEVICE_BRINGUP_YEAR=2018
 
 # Load extract_utils and do some sanity checks
 MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-LINEAGE_ROOT="$MY_DIR"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
-if [ ! -f "$HELPER" ]; then
-    echo "Unable to find helper script at $HELPER"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+if [ ! -f "${HELPER}" ]; then
+    echo "Unable to find helper script at ${HELPER}"
     exit 1
 fi
-. "$HELPER"
+source "${HELPER}"
 
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
-while [ "$1" != "" ]; do
-    case $1 in
-        -n | --no-cleanup )     CLEAN_VENDOR=false
-                                ;;
-        -s | --section )        shift
-                                SECTION=$1
-                                CLEAN_VENDOR=false
-                                ;;
-        * )                     SRC=$1
-                                ;;
+KANG=
+SECTION=
+
+while [ "${#}" -gt 0 ]; do
+    case "${1}" in
+        -n | --no-cleanup )
+                CLEAN_VENDOR=false
+                ;;
+        -k | --kang )
+                KANG="--kang"
+                ;;
+        -s | --section )
+                SECTION="${2}"; shift
+                CLEAN_VENDOR=false
+                ;;
+        * )
+                SRC="${1}"
+                ;;
     esac
     shift
 done
 
-if [ -z "$SRC" ]; then
-    SRC=adb
+if [ -z "${SRC}" ]; then
+    SRC="adb"
 fi
 
 function blob_fixup() {
     case "${1}" in
-<<<<<<< HEAD
         product/etc/permissions/qcrilhook.xml)
             sed -i 's|/system/framework/qcrilhook.jar|/system/product/framework/qcrilhook.jar|g' "${2}"
-=======
-        lib64/libwfdnative.so)
-            "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
-            ;;
-        system_ext/etc/init/dpmd.rc)
-            sed -i "s|/system/product/bin/|/system/system_ext/bin/|g" "${2}"
-            ;;
-        system_ext/etc/permissions/com.qti.dpmframework.xml | system_ext/etc/permissions/dpmapi.xml)
-            sed -i "s|/system/product/framework/|/system/system_ext/framework/|g" "${2}"
-            ;;
-        system_ext/etc/permissions/qti_libpermissions.xml)
-            sed -i 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"
-            ;;
-        system_ext/etc/permissions/qcrilhook.xml)
-            sed -i 's|/product/framework/qcrilhook.jar|/system/system_ext/framework/qcrilhook.jar|g' "${2}"
-            ;;
-        system_ext/lib64/lib-imsvideocodec.so)
-            "${PATCHELF}" --add-needed "libui_shim.so" "${2}"
-            ;;
-        system_ext/lib64/libdpmframework.so)
-            "${PATCHELF}" --add-needed "libshim_dpmframework.so" "${2}"
-            ;;
-        vendor/bin/mm-qcamera-daemon)
-            ;&
-        vendor/lib/libmmcamera2_cpp_module.so)
-            ;&
-        vendor/lib/libmmcamera2_cpp_module.so)
-            ;&
-        vendor/lib/libmmcamera2_dcrf.so)
-            ;&
-        vendor/lib/libmmcamera2_iface_modules.so)
-            ;&
-        vendor/lib/libmmcamera2_imglib_modules.so)
-            ;&
-        vendor/lib/libmmcamera2_mct.so)
-            ;&
-        vendor/lib/libmmcamera2_pproc_modules.so)
-            ;&
-        vendor/lib/libmmcamera2_stats_algorithm.so)
-            ;&
-        vendor/lib/libmmcamera2_stats_modules.so)
-            ;&
-        vendor/lib/libmmcamera_imglib.so)
-            ;&
-        vendor/lib/libmmcamera_pdaf.so)
-            ;&
-        vendor/lib/libmmcamera_pdafcamif.so)
-            ;&
-        vendor/lib/libmmcamera_tintless_algo.so)
-            ;&
-        vendor/lib/libmmcamera_tintless_bg_pca_algo.so)
-            sed -i 's|/data/misc/camera|/data/vendor/qcam|g' "${2}"
-            ;;
-        vendor/lib/libmmcamera2_sensor_modules.so)
-            sed -i 's|/system/etc/camera|/vendor/etc/camera|g' "${2}"
-            sed -i 's|/data/misc/camera|/data/vendor/qcam|g' "${2}"
-            ;;
-        vendor/lib/libmmcamera_dbg.so)
-            sed -i 's|persist.camera.debug.logfile|persist.vendor.camera.dbglog|g' "${2}"
-            sed -i 's|/data/misc/camera|/data/vendor/qcam|g' "${2}"
-            ;;
-        vendor/bin/smart_charger)
-            "${PATCHELF}" --add-needed "liblog.so" "${2}"
-            ;;
-        vendor/lib64/libsettings.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
-            ;;
-        vendor/lib64/libwvhidl.so)
-            "${PATCHELF}" --replace-needed "libprotobuf-cpp-lite.so" "libprotobuf-cpp-lite-v29.so" "${2}"
->>>>>>> 42616d223 (kuntao: Update qcrilmsgtunnel & deps from daisy)
             ;;
     esac
 }
 
 # Initialize the helper
-setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
+setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
-extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
 
-BLOB_ROOT="$LINEAGE_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
+BLOB_ROOT="$ANDROID_ROOT"/vendor/"$VENDOR"/"$DEVICE"/proprietary
 
 for HIDL_BASE_LIB in $(grep -lr "android\.hidl\.base@1\.0\.so" $BLOB_ROOT); do
     patchelf --remove-needed android.hidl.base@1.0.so "$HIDL_BASE_LIB" || true
@@ -151,4 +78,4 @@ for HIDL_MANAGER_LIB in $(grep -lr "android\.hidl\.@1\.0\.so" $BLOB_ROOT); do
     patchelf --remove-needed android.hidl.manager@1.0.so "$HIDL_MANAGER_LIB" || true
 done
 
-"$MY_DIR"/setup-makefiles.sh
+"${MY_DIR}/setup-makefiles.sh"
