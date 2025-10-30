4414d355ec52d6487dca0217925bdc6e16608f51
Vitaliy Tomin <highwaystar.ru@gmail.com>
2025-10-30 06:09:03 -0700

    nx549j: Initial device tree * Merge from"HighwayStar/android_device_nubia_nx549j" and "HighwayStar/android_device_nubia_msm8953-common"
    Change-Id: I4792a2949f90856221f81bfc285201ecbe46ab0c

---
+++ libshims/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    bionic/bionic_time_conversions.cpp \
+    bionic/pthread_cond.cpp
+
+LOCAL_SHARED_LIBRARIES := libc
+
+LOCAL_MODULE := libshim_libc
+LOCAL_MODULE_TAGS := optional
+LOCAL_VENDOR_MODULE := true
+LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
+
