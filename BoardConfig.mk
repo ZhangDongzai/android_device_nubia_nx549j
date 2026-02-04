69d591f1a149c2ab8ac0ef5acbed4d3a657ca89f
ZhangDongzai <3187480352@qq.com>
2026-02-04 13:14:17 +0800

    nx549j: Remove TARGET_PROVIDES_LIBLIGHT
    * Liblight has been removed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,9 +154,6 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CONFIG := lineageos_nx549j_defconfig
 TARGET_KERNEL_SOURCE := kernel/nubia/msm8953
 
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 
