edd5f0047fd41d9af1fa3bfe2ba5c91e5510210d
ZhangDongzai <3187480352@qq.com>
2026-02-15 11:11:13 +0800

    nx549j: Remove depreceated hostapd configs
    * Remove dead things

--- device.mk
+++ device.mk
@@ -415,9 +415,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/fstman.ini:system/etc/wifi/fstman.ini
-
 # WiFi Display
 PRODUCT_BOOT_JARS += \
     WfdCommon
