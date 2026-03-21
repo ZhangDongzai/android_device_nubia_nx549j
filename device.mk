6c74e34530ac71a0d512ecb7f1889d0ed80ca1b0
ZhangDongzai <3187480352@qq.com>
2026-03-21 21:22:03 +0800

    nx549j: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>
    Change-Id: Ia0834f5215ff8c3b13effa591bd2f84d93419a06

--- device.mk
+++ device.mk
@@ -387,6 +387,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # Touch
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.nubia_8953
